975cbaea7aa803e93cf1d5646a7f6ca9e9669032
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2022-10-24 07:45:19 +0300

    Cycle While

---
+++ src/CyclesAndArrays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CyclesAndArrays {
+    public static void main (String[] args){
+        //cycle "while"
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (true)
+        {
+            if (input.equals("Exit"))
+            {
+                break;
+            }
+        }
+
+        //
+    }
+}
